1d7993d05b8a3fa6e840d704d8c7a300d567ab82
RustuGokalp <94008145+RustuGokalp@users.noreply.github.com>
2024-02-26 23:23:41 +0300

    Prepare development env and made router integration

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import Menu from "./components/Menu";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import WrongPath from "./components/WrongPath";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/menu" exact element={<Menu />} />
+        <Route path="/about-us" exact element={<About />} />
+        <Route path="/contact" exact element={<Contact />} />
+        <Route path="*" exact element={<WrongPath />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
