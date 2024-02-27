2dce63c8dcf513eb1835d7eaa35f450962407b23
RustuGokalp <94008145+RustuGokalp@users.noreply.github.com>
2024-02-27 23:55:55 +0300

    Create Menu and MenuItem components

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,26 @@
+import MenuItem from "../components/MenuItem";
+import { Data } from "../helpers/Data";
+import "../styles/Menu.css";
+
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div className="menu">
+      <h1 className="menuTitle">Our Awesome Burgers</h1>
+      <div className="menuList">
+        {Data.map((item, key) => {
+          return (
+            <MenuItem
+              key={key}
+              image={item.image}
+              name={item.name}
+              content={item.content}
+              price={item.price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Menu;
---
+++ src/helpers/Data.jsx
@@ -0,0 +1,88 @@
+import ZulaBurger from "../assets/hamburger.webp";
+
+export const Data = [
+  {
+    name: "Efso Burger",
+    image: ZulaBurger,
+    content:
+      "So we have gathered some of our favorite burger recipes, from cheddar-stuffed burgers to Hatch Chile Smash Burgers. ",
+    price: 300,
+  },
+  {
+    name: "Efso Burger",
+    image: ZulaBurger,
+    content:
+      "So we have gathered some of our favorite burger recipes, from cheddar-stuffed burgers to Hatch Chile Smash Burgers. ",
+    price: 300,
+  },
+  {
+    name: "Efso Burger",
+    image: ZulaBurger,
+    content:
+      "So we have gathered some of our favorite burger recipes, from cheddar-stuffed burgers to Hatch Chile Smash Burgers. ",
+    price: 300,
+  },
+  {
+    name: "Efso Burger",
+    image: ZulaBurger,
+    content:
+      "So we have gathered some of our favorite burger recipes, from cheddar-stuffed burgers to Hatch Chile Smash Burgers. ",
+    price: 300,
+  },
+  {
+    name: "Efso Burger",
+    image: ZulaBurger,
+    content:
+      "So we have gathered some of our favorite burger recipes, from cheddar-stuffed burgers to Hatch Chile Smash Burgers. ",
+    price: 300,
+  },
+  {
+    name: "Efso Burger",
+    image: ZulaBurger,
+    content:
+      "So we have gathered some of our favorite burger recipes, from cheddar-stuffed burgers to Hatch Chile Smash Burgers. ",
+    price: 300,
+  },
+  {
+    name: "Efso Burger",
+    image: ZulaBurger,
+    content:
+      "So we have gathered some of our favorite burger recipes, from cheddar-stuffed burgers to Hatch Chile Smash Burgers. ",
+    price: 300,
+  },
+  {
+    name: "Efso Burger",
+    image: ZulaBurger,
+    content:
+      "So we have gathered some of our favorite burger recipes, from cheddar-stuffed burgers to Hatch Chile Smash Burgers. ",
+    price: 300,
+  },
+  {
+    name: "Efso Burger",
+    image: ZulaBurger,
+    content:
+      "So we have gathered some of our favorite burger recipes, from cheddar-stuffed burgers to Hatch Chile Smash Burgers. ",
+    price: 300,
+  },
+  {
+    name: "Efso Burger",
+    image: ZulaBurger,
+    content:
+      "So we have gathered some of our favorite burger recipes, from cheddar-stuffed burgers to Hatch Chile Smash Burgers. ",
+    price: 300,
+  },
+  {
+    name: "Efso Burger",
+    image: ZulaBurger,
+    content:
+      "So we have gathered some of our favorite burger recipes, from cheddar-stuffed burgers to Hatch Chile Smash Burgers. ",
+    price: 300,
+  },
+  {
+    name: "Efso Burger",
+    image: ZulaBurger,
+    content:
+      "So we have gathered some of our favorite burger recipes, from cheddar-stuffed burgers to Hatch Chile Smash Burgers. ",
+    price: 300,
+  },
+];
