d6e5e68795a527e996cf8444c2aafda043d4b722
RustuGokalp <94008145+RustuGokalp@users.noreply.github.com>
2024-02-27 23:15:02 +0300

    Create home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+import BannerImage from "../assets/burger-background.jpeg";
+import "../styles/Home.css";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div
+      className="mainPage"
+      style={{ backgroundImage: `url(${BannerImage})` }}
+    >
+      <div className="order">
+        <Link to="/menu">
+          <button>ORDER</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
