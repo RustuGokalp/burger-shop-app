24ecaad39a333ca9f07c80293af8c9995d66edf8
RustuGokalp <94008145+RustuGokalp@users.noreply.github.com>
2024-02-29 17:20:51 +0300

    Create contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,26 @@
+import "../styles/Contact.css";
+import ZulaInside from "../assets/zula-inside.webp";
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact">
+      <div
+        className="leftSide"
+        style={{ backgroundImage: `url(${ZulaInside})` }}
+      ></div>
+      <div className="rightSide">
+        <h1>Contact Us</h1>
+        <form>
+          <label> Name Surname</label>
+          <input type="text" name="name" />
+          <label> Email</label>
+          <input type="text" name="email" />
+          <label> Your Message</label>
+          <textarea name="message" rows="6"></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
